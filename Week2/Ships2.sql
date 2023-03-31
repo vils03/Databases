47b23dd5c9aae348c09d3e78e69c14f9abd6be43
Velislava Krumova <95179167+vils03@users.noreply.github.com>
2023-03-31 16:33:06 +0200

    Update Ships2.sql

--- Week2/Ships2.sql
+++ Week2/Ships2.sql
@@ -12,5 +12,14 @@ SELECT name, displacement, numguns
 FROM (outcomes JOIN ships ON ship=name ) JOIN classes ON ships.class = classes.class
 WHERE battle = 'Guadalcanal';
 
-/**/
+/*Напишете заявка, която извежда имената на тези държави, които имат както
+бойни кораби, така и бойни крайцери*/
+SELECT DISTINCT c1.country
+FROM classes c1, classes c2
+WHERE c1.country = c2.country AND ((c1.type ='bb' AND c2.type ='bc') OR(c1.type ='bc' AND c2.type ='bb'));
 
+/*. Напишете заявка, която извежда имената на тези кораби, които са били
+повредени в една битка, но по-късно са участвали в друга битка.*/
+SELECT o1.ship
+FROM outcomes o1, outcomes o2, battles b1,  battles b2
+WHERE o1.battle = b1.name AND o2.battle = b2.name AND o1.result = 'damaged' AND b1.date<b2.date AND o1.ship = o2.ship;
