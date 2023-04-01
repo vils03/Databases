af2083156a9acb29c45ecab7a206b7da37ded6e7
Velislava Krumova <95179167+vils03@users.noreply.github.com>
2023-04-01 14:27:05 +0200

    Update queryShips4.sql

--- Week4/queryShips4.sql
+++ Week4/queryShips4.sql
@@ -21,3 +21,18 @@ SELECT country, name
 FROM (classes JOIN ships ON classes.class = ships.class) LEFT JOIN outcomes ON name= ship
 WHERE ship IS NULL
 ORDER BY country;
+
+/*Намерете имената на всички кораби с поне 7 оръдия, пуснати на вода
+през 1916, но наречете резултатната колона Ship Name.*/
+SELECT name AS 'Ship Name'
+FROM ships JOIN classes ON ships.class = classes.class
+WHERE numguns >= 7 AND launched=1916;
+
+/*Изведете имената на всички потънали в битка кораби, името и дата на
+провеждане на битките, в които те са потънали. Подредете резултата по
+име на битката.*/
+SELECT ship, name, date
+FROM outcomes JOIN battles ON battle = name
+WHERE result = 'sunk'
+ORDER BY name;
+
