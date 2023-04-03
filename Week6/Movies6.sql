05c23587aac0973e02d722bb34684d30559b5a0b
Velislava Krumova <95179167+vils03@users.noreply.github.com>
2023-04-03 18:44:55 +0200

    Update Movies6.sql

--- Week6/Movies6.sql
+++ Week6/Movies6.sql
@@ -26,3 +26,31 @@ GROUP BY studioname;
 SELECT starname, COUNT(movietitle) AS numMovies
 FROM starsin
 GROUP BY starname;
+
+/*Напишете заявка, която за всяко студио извежда име на студиото и заглавие
+на филма, излязъл последно на екран за това студио.*/
+SELECT studioname, title, year
+FROM movie m1
+WHERE year >= ALL(SELECT year FROM movie m2 WHERE m1.studioname= m2.studioname);
+
+/*Напишете заявка, която извежда името на най-младия актьор (мъж)*/
+SELECT name
+FROM moviestar
+WHERE birthdate = (SELECT MAX(birthdate) FROM moviestar WHERE gender = 'M');
+
+/*Напишете заявка, която извежда име на актьор и име на студио за тези
+актьори, участвали в най-много филми на това студио.*/
+SELECT studioname, starname, COUNT(starname) AS num_movies
+FROM movie JOIN starsin ON title = movietitle AND year = movieyear
+GROUP BY studioname, starname
+HAVING COUNT(movietitle)>= ALL 
+(SELECT COUNT(movietitle) FROM movie JOIN starsin ON title = movietitle AND year = movieyear
+GROUP BY studioname, starname);
+
+/*Напишете заявка, която извежда заглавие и година на филма, и брой на
+актьорите, участвали в този филм за тези филми с повече от двама актьори*/
+SELECT title, year, COUNT(starname) AS numActors
+FROM starsin JOIN movie ON title = movietitle AND year = movieyear
+GROUP BY title, year
+HAVING COUNT(starname) >2;
+
